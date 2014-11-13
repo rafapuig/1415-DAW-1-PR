769ab274915f0e48ec321732e95702429d253973
rafapuig <rafapuig.profe@gmail.com>
2014-11-13 03:08:05 +0100

    QuicksortDemo

---
+++ MisProgramas/Libro/U05 - Tipos Compuestos/Ordenacion/QuickSortDemo/Arrays.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion.TiposCompuestos.Ordenacion
+{
+    delegate void MostrarOrdenacionQuickSort<T>(T[] items, int central, int primero, int ultimo, int i, int j);
+
+    static class Arrays
+    {
+        public static void QuickSort<T>(this T[] items, int primero, int ultimo,
+            Action comparacion = null, 
+            Action asignacion = null, 
+            MostrarOrdenacionQuickSort<T> show = null)
+            where T:System.IComparable<T>
+        {
+            int central = (primero + ultimo) / 2;
+            T pivote = items[central];
+
+            int i = primero;
+            int j = ultimo;
+
+            do
+            {
+                if (comparacion != null) comparacion();
+                while (items[i].CompareTo( pivote) <0) i++;
+
+                if (comparacion != null) comparacion();
+                while (items[j].CompareTo(pivote)>0) j--;
+
+                if (show != null) show(items, central, primero, ultimo, i, j);
+
+                if (i <= j)
+                {
+
+                    T temp = items[i]; if (asignacion != null) asignacion();
+                    items[i] = items[j]; if (asignacion != null) asignacion();
+                    items[j] = temp; if (asignacion != null) asignacion();
+                    i++;
+                    j--;
+                }
+
+            } while (i <= j);
+
+            if (primero < j) QuickSort(items, primero, j, comparacion, asignacion, show);
+            if (i < ultimo) QuickSort(items, i, ultimo, comparacion, asignacion, show);
+        }
+    }
+
+}
---
+++ MisProgramas/Libro/U05 - Tipos Compuestos/Ordenacion/QuickSortDemo/QuicksortDemo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion.TiposCompuestos.Ordenacion
+{
+    static class QuickSortDemo
+    {
+        static Random alea = new Random();
+
+        static void Main()
+        {         
+            TestQuickSort();
+            TestQuicksortPerformance();
+            Console.ReadKey();
+        }
+           
+     
+        static void TestQuickSort()
+        {
+            int[] numeros = { 5, 9, 2, 4, 8, 1, 3, 7, 6 };//{ 12, 5, 3, 13, 2, 9, 7 };
+
+            numeros.QuickSort(0, numeros.Length - 1, null, null, MostrarOrdenacionQuickSort);
+
+            MostrarItems(numeros);         
+        }
+        
+        static void MostrarOrdenacionQuickSort(int[] items, int central, int primero, int ultimo, int i, int j)
+        {
+            ConsoleColor colorPrevio = Console.ForegroundColor;
+            ConsoleColor fondoPrevio = Console.BackgroundColor;
+
+            for (int n = 0; n < items.Length; n++)
+            {
+                //Console.Write(" ");
+
+                if (n >= primero && n <= ultimo)
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+                if (n == i) Console.ForegroundColor = ConsoleColor.Red;
+
+                if (n == j) Console.ForegroundColor = ConsoleColor.Green;
+
+                if (n == central) Console.BackgroundColor = ConsoleColor.DarkYellow;
+
+                Console.Write(" {0,3}", items[n]);
+
+                Console.ForegroundColor = colorPrevio;
+                Console.BackgroundColor = fondoPrevio;
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        
+        public static void MostrarItems<T>(T[] elems)
+        {
+            Console.WriteLine(String.Join(", ", elems));
+            //Array.ForEach(elems, elem => Console.Write("{0} ", elem));
+            //Console.WriteLine();
+        }
+
+
+
+        static int[] CrearArray(int totalItems)
+        {
+            int[] elems = new int[totalItems];
+            for (int i = 0; i < elems.Length; i++)
+            {
+                elems[i] = alea.Next(0, totalItems);
+            }
+            return elems;
+        }                     
+        
+        static void TestQuicksortPerformance()
+        {
+            int talla = 5000000;
+            int[] valores = CrearArray(talla);
+
+            ulong contadorComparaciones = 0;
+            ulong contadorAsignaciones = 0;
+
+            System.Diagnostics.Stopwatch cronometro = System.Diagnostics.Stopwatch.StartNew();
+            
+            Arrays.QuickSort(valores, 0, valores.Length - 1,
+                () => contadorComparaciones++,
+                () => contadorAsignaciones++);
+
+            Console.WriteLine("Tiempo transcurrido {0}", cronometro.Elapsed);
+
+            Console.WriteLine("Total Comparaciones {0,4:N0}", contadorComparaciones);
+            Console.WriteLine("Total Asignaciones  {0,4:N0}", contadorAsignaciones);
+
+            Console.WriteLine("\nTeoricamente:");
+            Console.WriteLine("Coste n * Log2({0}) = {1:N0}", talla, talla * Math.Log10(talla) / Math.Log10(2));
+        }
+                
+    }
+
+}
